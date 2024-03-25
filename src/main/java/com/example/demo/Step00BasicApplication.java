75545475cf52e02187d10c574552a86bfa9942c1
kanghyekyoung10 <kanghyekoung3694@gmail.com>
2024-03-25 10:58:24 +0900

    init sourc commit

---
+++ src/main/java/com/example/demo/Step00BasicApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step00BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step00BasicApplication.class, args);
+		System.out.println("서버 실행!!");
+	}
+
+}
